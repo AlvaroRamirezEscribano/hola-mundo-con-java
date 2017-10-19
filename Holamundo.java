6687e9db33a43791eddaf2d012190effa34ce95c
Alvaro <piomh@hotmail.com>
2017-10-19 08:56:38 +0200

    comentarios añadidos al codigo

--- Holamundo.java
+++ Holamundo.java
@@ -1,3 +1,9 @@
+/**
+* Muestra por pantalla la frase "¡Hola mundo!"
+*
+*@author AlvaroRamirez
+*/
+
 public class HolaMundo {
 	public static void main(string[] args) {
 		System.out.printIn("¡Hola mundo!");
